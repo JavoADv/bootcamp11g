38969aedce186711820ab61c078db8b5523f238e
JavoADv <jav.adv@hotmail.com>
2021-03-22 12:01:00 -0600

    Avances cartas finales

---
+++ s9javascript/js/main.js
@@ -0,0 +1,48 @@
+/*alert ("¡Hola Koders!")
+console.log("¡Hola, Koders!")
+
+var variable1 = "JavierADv is a datatype string"
+
+console.log( typeof variable1 )
+
+var variable2 = 27 
+
+console.log( typeof variable2 )
+
+var variable3 = {name: "Javier "}
+
+console.log ( typeof variable3)*/
+
+var num1 = 50
+var num2 = 122 
+
+console.log (num1 ++ )
+console.log (num1 --)
+console.log (num1 * num2)
+console.log (num1 % num2)
+console.log (num1 + num2)
+console.log (num1 - num2 )
+console.log (num1 / num2 )
+console.log ("Te amo, bby")
+
+var string1 = "Yo"
+var string2 = "también"
+var string3 = "te amo, Bby."
+
+document.write ("<h3>" + string1 + " " + string2 + " " + string3 + "</h3>")
+
+var  x = 5, y= 6, z=3
+console.log (x == 5, x == "5", x == 8 )
+console.log ( x === 5, x ==="5")
+console.log ( x != 5, x != 45)
+console.log ( x !== "5", x !== 5)
+
+console.log (y < 10 && z > 1)
+console.log (y === 6 && y >= 6)
+
+console.log ( y != 6 || z == "3" )
+
+console.log ( ! (y > z))
+console.log ( !(z==5))
+
+
